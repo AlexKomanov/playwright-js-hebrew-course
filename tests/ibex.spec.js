f3cdf5eda0222b8dd9420b77d7df69d54279d457
AlexKomanov <akomanov88@gmail.com>
2024-03-04 21:05:41 +0200

    ibex ibex

--- tests/ibex.spec.js
+++ tests/ibex.spec.js
@@ -1,6 +1,21 @@
 import test from "@playwright/test";
 
-test('Login with standard_user', async({page}) => {
-    await page.goto('https://sqa-350.ibex-ai.com/')
-    await page.waitForTimeout(5000);
-});
+test.describe.only("IBEX", () => {
+    test('ibex', async ({ page }) => {
+        await page.goto('https://sqa-350.ibex-ai.com/')
+        await page.waitForTimeout(5000);
+    });
+
+    test('ibex2', async ({ page }) => {
+        await page.goto('https://sqa-350.ibex-ai.com/')
+        await page.waitForTimeout(5000);
+    });
+
+    test('ibex3', async ({ page }) => {
+        await page.goto('https://sqa-350.ibex-ai.com/')
+        await page.waitForTimeout(5000);
+    });
+})
+
+
+
